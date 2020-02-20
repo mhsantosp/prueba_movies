013ac39774a1b72166fe9f4bdab0087b4262daf4
Maria H <Santos P>
2020-02-20 16:41:56 -0500

    Complemento proyecto: Filtro para el buscador.

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,7 @@
 //Consumo de la API
-const api = `https://www.omdbapi.com/?s=movies&apikey=6ede1f9f`
+const api = `https://www.omdbapi.com/?s=movies&apikey=6ede1f9f`;
+let textBusqueda = '';
+let apiSearch = '';
 
 //Contenido inicial
 const contenido = document.querySelector('#movies');
@@ -7,12 +9,13 @@ fetch(api)
   .then(function (response) {
     return response.json();
   })
+
   .then(function (myJson) {
     let cantidad = Object.keys(myJson.Search).length;
     while (!(cantidad % 3 === 0)) {
       cantidad = cantidad - 1;
     }
-
+    
     for (let i = 0; i < cantidad; i++) {
       console.log(myJson.Search[i]);
       contenido.innerHTML += 
@@ -27,6 +30,7 @@ fetch(api)
     }
   });
 
+
 // Llena lista de peliculas
 var filtro = document.querySelector('#listaBusqueda');
 function traerOpcionesBusqueda () {
@@ -38,14 +42,62 @@ function traerOpcionesBusqueda () {
     let cantidadFiltro = Object.keys(data.Search).length;
     filtro.innerHTML = '';
     for (let i = 0; i < cantidadFiltro; i++) {
-    console.log(data.Search[i]);
-    filtro.innerHTML += 
-      `<li id='contenidoLista' class="list-group-item d-flex" onclick="recargar(${i})">
-        <span>
-          <img id='fltPoster' src='${data.Search[i].Poster}'>
-          ${data.Search[i].Title}
-        </span>
-      </li>`
+      console.log(data.Search[i]);
+      filtro.innerHTML +=
+        `<li id='contenidoLista' class="list-group-item d-flex" onclick="recargar(${i})">
+          <span>
+            <img id='fltPoster' src='${data.Search[i].Poster}'>
+            ${data.Search[i].Title}
+          </span>
+        </li>`
     }
   })
+  document.getElementById('coinsidencias').style.display = 'block'
 }
+
+
+//Llenado del filtro
+var filtroLista = document.querySelector('#listaBusqueda');
+var filter = document.querySelector('#movies');
+let buscar = () => {
+  if (document.getElementById('search').style.display === 'none') {
+    document.getElementById('search').style.display = 'block';
+  }
+
+  else {
+    textBusqueda = $('#search').val();
+    apiSearch = `https://www.omdbapi.com/?s=` + `${textBusqueda}` + `&apikey=6ede1f9f`
+
+    fetch(apiSearch)
+      .then(function (response) {
+        return response.json();
+      })
+
+      .then(function(bdApi) {
+        let cantFiltro = Object.keys(bdApi.Search).length;
+        filtroLista.innerHTML = '';
+        filter.innerHTML = '';
+        for (let i = 0; i < cantFiltro; i++) {
+          const intro = document.getElementById("introduccion");
+          intro.style.display = 'none';
+          console.log(bdApi.Search[i]);
+          filtroLista.innerHTML += 
+            `<li id='contenidoLista' class="list-group-item d-flex" onclick="recargar(${i})">
+              <span>
+                <img id='fltPoster' src='${bdApi.Search[i].Poster}'>
+                ${bdApi.Search[i].Title}
+              </span>
+            </li>`
+          filter.innerHTML += 
+            `<div class='col-4 py-3'>
+              <div class='card align-items-center flex-column h-100'>
+                <img src='${bdApi.Search[i].Poster}' class='card-img-top poster' onclick="recargar(${i})">
+                <div class='card-body mt-auto p-2 d-flex' onclick="recargar(${i})">
+                  <h5 class='card-title'>${bdApi.Search[i].Title}</h5>
+                </div>
+              </div>
+            </div>`
+        }
+      })
+  }
+};
--- js/refres.js
+++ js/refres.js
@@ -13,26 +13,119 @@ function refresInicio() {
 }
 
 
-//Refresca para mostrar la opción selecionada
+//Refresca para mostrar la opción buscada
 function recargar(index) {
   const contenidoNuevo = document.querySelector('#movies');
-  fetch(api)
+  let onlyitem = '';
+
+  if (textBusqueda !== "") {
+    fetch(apiSearch)
     .then(function (response) {
       return response.json();
     })
     .then(function(refres) {
-      const intro = document.getElementById("introduccion");
-      intro.style.display = 'none';
-      console.log(refres.Search[index]);
-      contenidoNuevo.innerHTML = 
-      `<div class='col-4 py-3'>
-        <div class='card align-items-center flex-column h-100'>
-          <img src='${refres.Search[index].Poster}' class='card-img-top poster'>
-          <div class='card-body mt-auto p-2 d-flex'>
-            <h5 class='card-title'>${refres.Search[index].Title}</h5>
-          </div>
-        </div>
-      </div>`;
-      refresInicio();
+      console.log (refres.Search[index].Title);
+      onlyitem = `https://www.omdbapi.com/?t=`+`${refres.Search[index].Title}`+`&apikey=6ede1f9f`;
+      // console.log (onlyitem);
+      fetch(onlyitem)
+        .then(function (response2) {
+          return response2.json();
+        })
+        .then(function (refres2) {
+          const intro = document.getElementById("introduccion");
+          intro.style.display = 'none';
+          console.log(refres2);
+          let actores = refres2.Actors;
+          let arrayActores = actores.split(',');
+          actores = '';
+          for (let i = 0; i < 3; i++) {
+            if (i === 0) {
+              actores += arrayActores[i]
+            } else {
+              actores += ', ' + arrayActores[i]
+            }
+          }
+          contenidoNuevo.innerHTML =
+            `<div class='col-4 py-3'>
+                <div class='card align-items-center flex-column h-100'>
+                  <img src='${refres2.Poster}' class='card-img-top poster'>
+                  <div class='card-body mt-auto p-2'>
+                    <h6 class='card-title'>${refres2.Title}</h6>
+                    <span class='card-title'>
+                      <p class='mb-0' id='rating'><b>Rating: </b>${refres2.imdbRating}</p>
+                      <p class='mb-0' id='actores'><b>Actores: </b>${actores}</p>
+                    </span>
+                  </div>
+                </div>
+              </div>`;
+          refresInicio();
+        })
     })
+  }
+  
+  else {
+    fetch(api)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function(refres) {
+        console.log (refres.Search[index].Title);
+        onlyitem = `https://www.omdbapi.com/?t=`+`${refres.Search[index].Title}`+`&apikey=6ede1f9f`;
+        // console.log (onlyitem);
+        
+        fetch(onlyitem)
+          .then(function (response2) {
+            return response2.json();
+          })
+          .then(function (refres2) {
+            const intro = document.getElementById("introduccion");
+            intro.style.display = 'none';
+            console.log(refres2);
+            let actores = refres2.Actors;
+            let arrayActores = actores.split(',');
+            actores = '';
+            for (let i = 0; i < 3; i++) {
+              if (i === 0) {
+                actores += arrayActores[i]
+              } else {
+                actores += ', ' + arrayActores[i]
+              }
+            }
+            contenidoNuevo.innerHTML = 
+              `<div class='col-4 py-3'>
+                <div class='card align-items-center flex-column h-100'>
+                  <img src='${refres2.Poster}' class='card-img-top poster'>
+                  <div class='card-body mt-auto p-2'>
+                    <h6 class='card-title'>${refres2.Title}</h6>
+                    <span class='card-title'>
+                      <p class='mb-0' id='rating'><b>Rating: </b>${refres2.imdbRating}</p>
+                      <p class='mb-0' id='actores'><b>Actores: </b>${actores}</p>
+                    </span>
+                  </div>
+                </div>
+              </div>`;
+            refresInicio();
+          })
+      })
+  }
 }
+
+
+// function ocultar() {
+//   document.getElementById('search').style.display = 'none';
+//   document.getElementById('coinsidencias').style.display = 'none';
+// }
+// function ver() {
+//   document.getElementById('search').style.display = 'block';
+//   document.getElementById('coinsidencias').style.display = 'block';
+// }
+
+// //Paginación
+// $(document).ready(function() {
+//   $('#movies').pageMe({
+//     pagerSelector: '#page',
+//     showPrevNext: true,
+//     hidePageNumbers: false,
+//     perPage: 3
+//   });
+// });
